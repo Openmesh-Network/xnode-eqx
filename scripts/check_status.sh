e5848254d8d538bd9a029040a205aa0a7ecf0e15
Andrew Ong <andrew.hai.ong@gmail.com>
2023-09-06 16:34:31 +1000

    Replacing taints with replace

--- scripts/check_status.sh
+++ scripts/check_status.sh
@@ -4,7 +4,7 @@ export KUBECONFIG=./kubeconfig
 
 ATTEMPTS=0
 ROLLOUT_STATUS_CMD=$command
-until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -gt 60 ]; do
   $ROLLOUT_STATUS_CMD
   ATTEMPTS=$((attempts + 1))
   sleep 10
